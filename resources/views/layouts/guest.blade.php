8b7047136d72f67b8d10c1c64cb0c11e455a6916
Devfrank94 <francescomurro94@gmail.com>
2023-07-13 15:12:56 +0200

    partial commit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,11 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Deliveboo') }}</title>
-        
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest"> 
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
