196a966b3fc5869a8c8407947019216435da423e
Devfrank94 <francescomurro94@gmail.com>
2023-07-13 16:51:39 +0200

    create migration and model restaurant

---
+++ database/migrations/2023_07_13_141118_create_restaurants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('slug', 255)->unique();
+            $table->string('address', 255);
+            $table->string('p_iva', 45);
+            $table->tinyInteger('visible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
