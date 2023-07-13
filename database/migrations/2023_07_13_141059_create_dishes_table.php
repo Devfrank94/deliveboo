c1a0df59765b70dc01da168edb02b32641fdf975
samuelebumbello <samuelebumbello17@gmail.com>
2023-07-13 16:51:45 +0200

    create migration and model dishe

---
+++ database/migrations/2023_07_13_141059_create_dishes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name' , 255);
+            $table->string('slug' , 255);
+            $table->decimal('price', $precision = 7, $scale = 2);
+            $table->text('description');
+            $table->text('ingredients');
+            $table->tinyInteger('visible');
+            $table->tinyInteger('vote');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
