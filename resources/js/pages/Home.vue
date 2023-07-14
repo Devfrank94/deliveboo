f8c510d02135d6535bd541f6a28b8cea94b122ae
samuelebumbello <samuelebumbello17@gmail.com>
2023-07-14 10:51:11 +0200

    create structure section front end

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -8,9 +8,40 @@ export default {
 
     <h1>Pages Home</h1>
 
+    <section class="jumbotron test">
+
+    </section>
+
+    <section id="sec-1" class="test2"></section>
+    <section id="sec-2 " class="test"></section>
+    <section id="sec-3" class="test3"></section>
+    <section id="sec-4" class="test2"></section>
+
 </template>
 
 
-<style>
+<style lang="scss" scoped>
+
+.jumbotron{
+    height: 400px;
+}
+
+#sec-1{
+    height: 400px;
+}
+
+#sec-2{
+    height: 400px;
+}
+
+#sec-3{
+    height: 400px;
+}
+
+#sec-4{
+    height: 400px;
+}
+
+
 
 </style>
