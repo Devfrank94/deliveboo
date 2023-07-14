962404cdb9b80c0dc8ce830715571a6c69dbdf46
nico17022001 <nico17022001boolean@gmail.com>
2023-07-14 10:42:41 +0200

    create many-to-one dishes restaurants

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -1,11 +1,21 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Dish extends Model
 {
     use HasFactory;
+
+    public function restaurant()
+        {
+            return $this->belongsTo(Restaurant::class);
+        }
+
 }
+
+
+
+
--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurant extends Model
 {
     use HasFactory;
+    public function dishes()
+    {
+        return $this->hasMany(Dish::class);
+    }
 }
