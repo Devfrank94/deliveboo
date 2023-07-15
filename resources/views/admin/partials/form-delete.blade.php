06d2881a5ddb15313f10fba4f9cda883f2d1dd51
Devfrank94 <francescomurro94@gmail.com>
2023-07-15 16:29:13 +0200

    complete show

--- resources/views/admin/partials/form-delete.blade.php
+++ resources/views/admin/partials/form-delete.blade.php
@@ -8,7 +8,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5 text-danger" id="exampleModalLabel">Alert!!!</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
