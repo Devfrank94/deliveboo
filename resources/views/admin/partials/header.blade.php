b93032bcc548d52cade407c1021d9ccdb0a17a2b
Devfrank94 <francescomurro94@gmail.com>
2023-07-16 22:04:43 +0200

    complete create blade

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -16,7 +16,7 @@
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    {{-- scritta in questo modo richiama varivbili per sito multilanguage
+                    {{-- scritta in questo modo richiama variabili per sito multilanguage
                     <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a> --}}
                     <a class="nav-link" target="_blank" href="{{ route('home') }}">Home</a>
                 </li>
