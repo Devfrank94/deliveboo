34ad1ee5b4e084cd432a4b06040412c4e41f734e
Sara Gisondi <sara.gisondi@outlook.it>
2023-07-19 11:24:31 +0200

    create orders

---
+++ config/orders.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+  [
+    "firstname_user"=> "Raffaella",
+    "lastname_user"=> "Cerullo",
+    "email_user"=> "lila.cerullo@gmail.com",
+    "phone_number_user"=> "3467591187",
+    "address_user"=> "Via Alessandria, 110, 00198 Roma RM",
+    "type_payment"=> "bancomat",
+    "total_price"=> "20,00",
+  ],
+
+  [
+    "firstname_user"=> "Elena",
+    "lastname_user"=> "Greco",
+    "email_user"=> "lenu.greco@aruba.com",
+    "phone_number_user"=> "3401145325",
+    "address_user"=> "Via dei Magazzini Generali, 4, 00154 Roma RM",
+    "type_payment"=> "bancomat",
+    "total_price"=> "12,30",
+  ],
+
+  [
+    "firstname_user"=> "Michele",
+    "lastname_user"=> "Solara",
+    "email_user"=> "m.solara@outlook.com",
+    "phone_number_user"=> "3440325534",
+    "address_user"=> "Via Principe Amedeo, 14, 00185 Roma RM",
+    "type_payment"=> "bancomat",
+    "total_price"=> "35,80",
+  ],
+
+  [
+    "firstname_user"=> "Nino",
+    "lastname_user"=> "Sarratore",
+    "email_user"=> "nino-sarratore@libero.com",
+    "phone_number_user"=> "3452399677",
+    "address_user"=> "Via Giovanni Prati, 1, 00152 Roma RM",
+    "type_payment"=> "bancomat",
+    "total_price"=> "15,50",
+  ],
+
+  [
+    "firstname_user"=> "Stefano",
+    "lastname_user"=> "Carracci",
+    "email_user"=> "ste.carra@virgilio.com",
+    "phone_number_user"=> "3407665360",
+    "address_user"=> "Via Savoia, 44, 00198 Roma RM",
+    "type_payment"=> "bancomat",
+    "total_price"=> "40,00",
+  ]
+
+];
