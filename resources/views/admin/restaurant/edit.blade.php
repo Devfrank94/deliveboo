fd48aad791ffbb8c85af921b67e428bd6a839e85
CosimoTramonte <cosimotramonte7@gmail.com>
2023-07-19 12:46:19 +0200

    add edit restaurant

--- resources/views/admin/restaurant/edit.blade.php
+++ resources/views/admin/restaurant/edit.blade.php
@@ -1,18 +1,21 @@
 @extends('layouts.admin')
 
 @section('content')
-  <div class="container mt-5 ">
-    <form action="{{route('admin.restaurant.update', $restaurant)}}" method="POST" enctype="multipart/form-data">
+  <div class="container mt-5 ms-4">
+    <h4 class="fs-4 text-secondary my-4">
+      Modifica il mio Ristorante
+    </h4>
+    <form onsubmit="return validateCheckbox()" action="{{route('admin.restaurant.update', $restaurant)}}" method="POST" enctype="multipart/form-data">
       @csrf
       @method('PUT')
 
       <div class="">
-        <label for="name_restaurant" class="col-md-4 col-form-label text-md-right">{{ __('Name Restaurants') }}</label>
+        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name Restaurants') }}</label>
 
         <div class="col-md-6">
-            <input id="name_restaurant" type="text" class="form-control @error('name_restaurant') is-invalid @enderror" name="name_restaurant" value="{{ old('name_restaurant') }}" required autocomplete="name_restaurant" autofocus>
+            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name', $restaurant->name) }}" required autocomplete="name" autofocus>
 
-            @error('name_restaurant')
+            @error('name')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
@@ -29,15 +32,86 @@
             id="image"
             name="image"
         >
-        <img class="py-3" src="" id="img-show" alt="" width="200">
+        <img class="py-3" src="{{ asset('storage/' . $restaurant->image_path)}}" id="img-show" alt="" width="200">
       </div>
 
+      <div class="mt-4">
+        <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Address Restaurants') }}</label>
 
+        <div class="col-md-6">
+            <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address', $restaurant->address) }}" required autocomplete="address" autofocus>
+
+            @error('address')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+    <div class="mt-4">
+        <label for="p_iva" class="col-md-4 col-form-label text-md-right">{{ __('P. Iva Restaurants') }}</label>
+
+        <div class="col-md-6">
+            <input id="p_iva" type="text" class="form-control @error('p_iva') is-invalid @enderror" name="p_iva" value="{{ old('p_iva', $restaurant->p_iva) }}" required autocomplete="p_iva" autofocus>
+
+            @error('p_iva')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+
+    <div class="mt-5">
+      <div class="form-check form-switch mb-3">
+        <label for="visible" class="visible">Vuoi attivare il tuo Ristorante?</label>
+        <input
+          class="form-check-input"
+          type="checkbox"
+          role="switch"
+          name="visible"
+          id="visible"
+          value="{{ old('visible', $restaurant?->visible)}}" @if($restaurant?->visible)
+          checked
+        @endif>
+      </div>
+    </div>
+
+    <div class="mb-3">
+      <label class="col-md-4 col-form-label text-md-right">Typologies</label>
+      <div class="btn-group d-flex flex-wrap w-50" role="group" aria-label="Basic checkbox toggle button group">
+          @foreach ($typologies as $typology)
+              <input
+                  type="checkbox"
+                  class="btn-check form-control @error('typologies[]') is-invalid @enderror"
+                  id="typology{{$loop->iteration}}"
+                  value="{{$typology->id}}"
+                  name="typologies[]"
+                  autocomplete="off"
+                  @if (!$errors->any() && $restaurant->typologies->contains($typology))
+                    checked
+                  @elseif ($errors->any() && in_array($typology->id, old('typologies', [])))
+                      checked
+                  @endif
+                >
+                <label class="btn btn-outline-secondary m-2" for="typology{{$loop->iteration}}">{{ $typology->name }}</label>
+          @endforeach
+<!-- CONTROLLARE INPUT ERRORE DAL BACK-END, SEMBRA LEGGERE L'ERRORE MA NON LO FA VEDERE IN PAGINA-->
+            @error('typologies[]')
+              <span class="invalid-feedback" role="alert">
+                  <strong>Selezionare almeno una tipologia</strong>
+              </span>
+            @enderror
+      </div>
+    </div>
+
+    <button type="submit" class="btn btn-success mt-3 me-3">Salva</button>
+    <a href="{{route('admin.restaurant.index')}}" class="btn btn-dark mt-3">Indietro</a>
     </form>
   </div>
 
 
-  <script>
+  <script language="javascript" type="text/javascript">
     ClassicEditor
         .create( document.querySelector( '#description' ) )
         .catch( error => {
@@ -48,5 +122,17 @@
         const tagImg = document.getElementById('img-show');
         tagImg.src = URL.createObjectURL(event.target.files[0]);
     }
+
+    //function validateCheckbox(){
+    //  var form_data = new FormData(document.querySelector("form"));
+    //  if(form_data.has("typologies[]")){
+    //    return true;
+    //  }
+    //  else{
+    //    console.log(form_data.has("typologies[]"));
+    //    alert("Devi scegliere almeno una tipologia!")
+    //    return false;
+    //  }
+    //}
   </script>
 @endsection
