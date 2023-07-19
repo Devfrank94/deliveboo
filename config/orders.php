7cb351d6b24d7e77fa98f9188ecf8b95ba67a040
Sara Gisondi <sara.gisondi@outlook.it>
2023-07-19 16:58:59 +0200

    add array orders

--- config/orders.php
+++ config/orders.php
@@ -3,53 +3,53 @@
 return [
 
   [
-    "firstname_user"=> "Raffaella",
-    "lastname_user"=> "Cerullo",
+    "surname_user"=> "Raffaella",
+    "name_user"=> "Cerullo",
     "email_user"=> "lila.cerullo@gmail.com",
     "phone_number_user"=> "3467591187",
     "address_user"=> "Via Alessandria, 110, 00198 Roma RM",
     "type_payment"=> "bancomat",
-    "total_price"=> "20,00",
+    "total_price"=> "20.00",
   ],
 
   [
-    "firstname_user"=> "Elena",
-    "lastname_user"=> "Greco",
+    "surname_user"=> "Elena",
+    "name_user"=> "Greco",
     "email_user"=> "lenu.greco@aruba.com",
     "phone_number_user"=> "3401145325",
     "address_user"=> "Via dei Magazzini Generali, 4, 00154 Roma RM",
     "type_payment"=> "bancomat",
-    "total_price"=> "12,30",
+    "total_price"=> "12.30",
   ],
 
   [
-    "firstname_user"=> "Michele",
-    "lastname_user"=> "Solara",
+    "surname_user"=> "Michele",
+    "name_user"=> "Solara",
     "email_user"=> "m.solara@outlook.com",
     "phone_number_user"=> "3440325534",
     "address_user"=> "Via Principe Amedeo, 14, 00185 Roma RM",
     "type_payment"=> "bancomat",
-    "total_price"=> "35,80",
+    "total_price"=> "35.80",
   ],
 
   [
-    "firstname_user"=> "Nino",
-    "lastname_user"=> "Sarratore",
+    "surname_user"=> "Nino",
+    "name_user"=> "Sarratore",
     "email_user"=> "nino-sarratore@libero.com",
     "phone_number_user"=> "3452399677",
     "address_user"=> "Via Giovanni Prati, 1, 00152 Roma RM",
     "type_payment"=> "bancomat",
-    "total_price"=> "15,50",
+    "total_price"=> "15.50",
   ],
 
   [
-    "firstname_user"=> "Stefano",
-    "lastname_user"=> "Carracci",
+    "surname_user"=> "Stefano",
+    "name_user"=> "Carracci",
     "email_user"=> "ste.carra@virgilio.com",
     "phone_number_user"=> "3407665360",
     "address_user"=> "Via Savoia, 44, 00198 Roma RM",
     "type_payment"=> "bancomat",
-    "total_price"=> "40,00",
+    "total_price"=> "40.00",
   ]
 
 ];
