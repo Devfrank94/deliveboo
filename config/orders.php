5d9041a68d55276e3064ca97ed89d1d8a6a17858
Sara Gisondi <sara.gisondi@outlook.it>
2023-07-20 17:04:02 +0200

    modify payment

--- config/orders.php
+++ config/orders.php
@@ -8,7 +8,7 @@ return [
     "email_user"=> "lila.cerullo@gmail.com",
     "phone_number_user"=> "3467591187",
     "address_user"=> "Via Alessandria, 110, 00198 Roma RM",
-    "type_payment"=> "bancomat",
+    "type_payment"=> "carta",
     "total_price"=> "20.00",
   ],
 
@@ -18,7 +18,7 @@ return [
     "email_user"=> "lenu.greco@aruba.com",
     "phone_number_user"=> "3401145325",
     "address_user"=> "Via dei Magazzini Generali, 4, 00154 Roma RM",
-    "type_payment"=> "bancomat",
+    "type_payment"=> "carta",
     "total_price"=> "12.30",
   ],
 
@@ -28,7 +28,7 @@ return [
     "email_user"=> "m.solara@outlook.com",
     "phone_number_user"=> "3440325534",
     "address_user"=> "Via Principe Amedeo, 14, 00185 Roma RM",
-    "type_payment"=> "bancomat",
+    "type_payment"=> "carta",
     "total_price"=> "35.80",
   ],
 
@@ -38,7 +38,7 @@ return [
     "email_user"=> "nino-sarratore@libero.com",
     "phone_number_user"=> "3452399677",
     "address_user"=> "Via Giovanni Prati, 1, 00152 Roma RM",
-    "type_payment"=> "bancomat",
+    "type_payment"=> "carta",
     "total_price"=> "15.50",
   ],
 
@@ -48,7 +48,7 @@ return [
     "email_user"=> "ste.carra@virgilio.com",
     "phone_number_user"=> "3407665360",
     "address_user"=> "Via Savoia, 44, 00198 Roma RM",
-    "type_payment"=> "bancomat",
+    "type_payment"=> "carta",
     "total_price"=> "40.00",
   ]
 
