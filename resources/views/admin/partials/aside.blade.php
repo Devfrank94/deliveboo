31ceab32d7baf3085393e7931c528826d8960a7e
nico17022001 <nico17022001boolean@gmail.com>
2023-07-27 12:43:34 +0200

    add fix icon

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -3,19 +3,19 @@
 <nav class="mt-4 me-1">
 <ul>
     <li class="mb-3">
-        <a href="{{route('admin.home')}}"><i class="fa-solid fa-gauge-high me-2"></i> Dashboard</a>
+        <a href="{{route('admin.home')}}"><i class="fa-solid fa-gauge-high"></i><span class="dis-span">Dashboard</span></a>
     </li>
     <li class="mb-3">
-        <a href="{{route('admin.orders.index')}}"><i class="fa-solid fa-chart-line me-2"></i> Ordini</a>
+        <a href="{{route('admin.orders.index')}}"><i class="fa-solid fa-chart-line"></i><span class="dis-span">Ordini</span></a>
     </li>
     <li class="mb-3">
-        <a href="{{route('admin.statistics')}}"><i class="fa-solid fa-chart-column me-2"></i> Statistiche</a>
+        <a href="{{route('admin.statistics')}}"><i class="fa-solid fa-chart-column"></i><span class="dis-span">Statistiche</span></a>
     </li>
     <li class="mb-3">
-        <a href="{{route('admin.restaurant.index')}}"><i class="fa-solid fa-building-user me-2"></i> Il mio Ristorante</a>
+        <a href="{{route('admin.restaurant.index')}}"><i class="fa-solid fa-building-user"></i><span class="dis-span">Il mio Ristorante</span></a>
     </li>
     <li class="mb-3">
-    <a href="{{route('admin.dishes.index')}}"><i class="fa-solid fa-utensils"></i> Piatti</a>
+    <a href="{{route('admin.dishes.index')}}"><i class="fa-solid fa-utensils"></i><span class="dis-span">Piatti</span></a>
     </li>
 </ul>
 </nav>
