a179198cdf03f138335491580ca6b9e0d77a6dd4
nico17022001 <nico17022001boolean@gmail.com>
2023-07-27 13:02:01 +0200

    fix header dashboard icon

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -2,6 +2,10 @@
 
 <nav class="mt-4 me-1">
 <ul>
+    <li class="mb-3 dis-link">
+      <a class="nav-link" target="_blank" href="{{ route('home') }}"><i class="fa-solid fa-house" style="color: #ffffff;"></i></a>
+    </li>
+
     <li class="mb-3">
         <a href="{{route('admin.home')}}"><i class="fa-solid fa-gauge-high"></i><span class="dis-span">Dashboard</span></a>
     </li>
@@ -16,6 +20,11 @@
     </li>
     <li class="mb-3">
     <a href="{{route('admin.dishes.index')}}"><i class="fa-solid fa-utensils"></i><span class="dis-span">Piatti</span></a>
+    </li>
+
+    <li class="mb-3 dis-link">
+      <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+      <i class="fa-solid fa-arrow-right-from-bracket"></i></a>
     </li>
 </ul>
 </nav>
